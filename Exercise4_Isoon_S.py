d3b66c6a9b50a1cb150d54c9ff39555c687c25b1
Retepppppp <personal.retep@gmail.com>
2023-03-24 17:52:16 +0700

    Exercise

---
+++ Exercise4_Isoon_S.py
@@ -0,0 +1,12 @@
+'''variable'''
+FE = 80.0
+GB = 82.5
+CS = 79.2
+CP = 89.7
+
+print("---Your Score---")
+
+print("Foundation English :", "คะแนนที่ได้ :",FE)
+print("General Business :", "คะแนนที่ได้ :",GB)
+print("Introduction to Computer Systems :", "คะแนนที่ได้ :",CS)
+print("Computer Programming :", "คะแนนที่ได้ :",CP)
---
+++ Exercise5_1_Isoon_S.py
@@ -0,0 +1,7 @@
+Num1 = int(input())
+Num2 = int(input())
+
+print(Num1, "+" ,Num2, "=", Num1 + Num2)
+print(Num1, "-" ,Num2, "=", Num1 - Num2)
+print(Num1, "*" ,Num2, "=", Num1 * Num2)
+print(Num1, "/" ,Num2, "=", Num1 / Num2)
